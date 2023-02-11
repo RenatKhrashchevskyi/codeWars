2a023efe592a4654cc0b2a37f3a1394488bcebc6
Renat Khrashchevskyi <renatkhrashchevskyi@Renats-MacBook-Air.local>
2023-02-10 17:47:09 -0700

    first commit

---
+++ 8kyu.js
@@ -0,0 +1,22 @@
+//Simple multiplication
+function simpleMultiplication(number) {
+    return number % 2 === 0 ? number * 8: number * 9;
+   
+ }
+
+//If you can't sleep, just count sheep!!
+const countSheep = (num) => {
+    let sheep = ''
+       for (let i = 1; i <= num; i++){
+         sheep += i + ' sheep...'
+       }
+      return sheep;
+    }
+
+
+// Simple multiplication
+
+function simpleMultiplication(number) {
+  return number % 2 === 0 ? number * 8: number * 9;
+ 
+}
