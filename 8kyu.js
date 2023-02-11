1884bc177e0439784ca3e557d397e9030bb97a1a
Renat Khrashchevskyi <renathraschevskiy@gmail.com>
2023-02-10 19:45:38 -0700

    8kyu

--- 8kyu.js
+++ 8kyu.js
@@ -13,10 +13,15 @@ const countSheep = (num) => {
       return sheep;
     }
 
-
 // Simple multiplication
-
 function simpleMultiplication(number) {
   return number % 2 === 0 ? number * 8: number * 9;
 
 }
+
+//Filter out the geese
+
+const gooseFilter = (birds) => {
+  let geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+  return birds.filter((item) => !geese.includes(item))
+};
