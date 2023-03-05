2c94994c7c94a755bbeaec4f887da84c1fc3b4ae
Renat Khrashchevskyi <renathraschevskiy@gmail.com>
2023-03-05 15:57:00 -0700

    8kyu

--- 8kyu.js
+++ 8kyu.js
@@ -1,78 +1,86 @@
 //Simple multiplication
 function simpleMultiplication(number) {
-    return number % 2 === 0 ? number * 8: number * 9;
-   
- }
+  return number % 2 === 0 ? number * 8 : number * 9;
+}
 
 //If you can't sleep, just count sheep!!
 const countSheep = (num) => {
-    let sheep = ''
-       for (let i = 1; i <= num; i++){
-         sheep += i + ' sheep...'
-       }
-      return sheep;
-    }
+  let sheep = "";
+  for (let i = 1; i <= num; i++) {
+    sheep += i + " sheep...";
+  }
+  return sheep;
+};
 
 // Simple multiplication
 function simpleMultiplication(number) {
-  return number % 2 === 0 ? number * 8: number * 9;
-
+  return number % 2 === 0 ? number * 8 : number * 9;
 }
 
 //Filter out the geese
 
 const gooseFilter = (birds) => {
   let geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
-  return birds.filter((item) => !geese.includes(item))
+  return birds.filter((item) => !geese.includes(item));
 };
-
 
 //Even or Odd
 
 let evenOrOdd = (number) => {
-  return number % 2 == 0 ? 'Even' : 'Odd'
-}
+  return number % 2 == 0 ? "Even" : "Odd";
+};
 
 // Fun with ES6 Classes #1 - People, people, people
 
-
 class Person {
-  constructor(firstName = 'John',lastName = 'Doe',age = 0,gender = 'Male'){
+  constructor(firstName = "John", lastName = "Doe", age = 0, gender = "Male") {
     this.firstName = firstName;
     this.lastName = lastName;
     this.age = age;
     this.gender = gender;
-    
-Person.greetExtraTerrestrials = function(raceName){
+
+    Person.greetExtraTerrestrials = function (raceName) {
       return `Welcome to Planet Earth ${raceName}`;
-};
+    };
   }
-  sayFullName(){
+  sayFullName() {
     return `${this.firstName} ${this.lastName}`;
   }
-
 }
 
 // Make an array of numbers that are doubles of the first array
 
-
-let double = arr => arr.map(i => i * 2)
+let double = (arr) => arr.map((i) => i * 2);
 console.log(double([2, 3, 5]));
-
 
 // Take an array of numbers and make them strings
 
 function someArr(arr) {
-  return arr.map(i => i.toString())
+  return arr.map((i) => i.toString());
 }
-console.log(someArr([2, 3 ,5]))
+console.log(someArr([2, 3, 5]));
 
+// // // Capitalize each of an array of names
+function capitalizeNames(arr) {
+  //return arr.map(i => i.toUpperCase().charAt(0) + i.toLowerCase())
+  return arr.map((i, ind) => i.charAt(0, 1).toUpperCase() + i.slice(1));
+}
 
-// Capitalize each of an array of names
+console.log(capitalizeNames(["john", "JACOB", "jinGleHeimer", "schmidt"]));
 
-let capitLetter = arr => arr.map(word => word.charAt(0).toUpperCase() + word.slice(1)) 
-console.log(capitLetter(['hello', 'jonny']));
+// Make an array of strings of the names
 
+function namesOnly(arr) {
+return arr.map(i => i.name)
+}
 
+console.log(namesOnly([{name: "Angelina Jolie", age: 80,},{name: "Eric Jones", age: 2,},{name: "Paris Hilton",age: 5,},]));
 
+//Make an array of strings of the names saying whether or not they can go to The Matrix
+
+function makeStrings(arr){
+  return arr.map(i => i.age >= 18 ? `${i.name} can go to The Matrix ` : `${i.name} is under age!!`)
+}
+
+console.log(makeStrings([{name: "Angelina Jolie", age: 80}, {name: "Eric Jones", age: 2}]))
 
